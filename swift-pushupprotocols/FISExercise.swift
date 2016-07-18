35dfa918b7cdc9f410178edd5f0cebf4d028f7a9
Johann Kerr <johannkerr@gmail.com>
2016-07-18 02:24:19 -0400

    first commit

---
+++ swift-pushupprotocols/FISExercise.swift
@@ -0,0 +1,9 @@
+//
+//  FISExercise.swift
+//  swift-pushupprotocols
+//
+//  Created by Johann Kerr on 7/10/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import Foundation
---
+++ swift-pushupprotocols/FISWorkout.swift
@@ -0,0 +1,9 @@
+//
+//  FISWorkout.swift
+//  swift-pushupprotocols
+//
+//  Created by Johann Kerr on 7/10/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import Foundation
